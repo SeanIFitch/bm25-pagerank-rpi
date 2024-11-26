b3501391442264e3d05da3e05b51f5438c91aace
Sean Fitch <seanifitch@gmail.com>
2024-11-26 10:33:37 -0500

    Update daragen script to use 1 file

--- cmd/datagen/main.go
+++ cmd/datagen/main.go
@@ -10,25 +10,18 @@ import (
 // Create microsoft pairwise comparison dataset
 // Designed to use data from https://www.microsoft.com/en-us/research/project/mslr/ by Tao Qin and Tie-Yan Liu
 func main() {
-	trainFile := flag.String("trainFile", "", "Path to the train dataset file (e.g., MSLR-WEB30K/Fold1/train.txt)")
-	testFile := flag.String("testFile", "", "Path to the test dataset file (e.g., MSLR-WEB30K/Fold1/test.txt)")
-	trainSave := flag.String("saveTrainFile", "", "Path to the file in which to save the train dataset (e.g., data/processed/MSLR-WEB30K/Fold1/1mil-train.gob)")
-	testSave := flag.String("saveTestFile", "", "Path to the file in which to save the test dataset (e.g., data/processed/MSLR-WEB30K/Fold1/100k-test.gob)")
-	trainCount := flag.Int("trainCount", 1000000, "Number of train examples to save")
-	testCount := flag.Int("testCount", 100000, "Number of test examples to save")
+	file := flag.String("file", "", "Path to the train dataset file (e.g., MSLR-WEB30K/Fold1/train.txt)")
+	csvSave := flag.String("csvFile", "", "Path to the file in which to save the file as CSV (e.g., data/processed/MSLR-WEB30K/Fold1/1mil-train.csv)")
+	gobSave := flag.String("gobFile", "", "Path to the file in which to save the file as gob (e.g., data/processed/MSLR-WEB30K/Fold1/1mil-train.gob)")
+	exampleCount := flag.Int("trainCount", 1000000, "Number of examples to save")
 	minDiff := flag.Int("minDiff", 3, "Minimum relevance difference for a valid example")
-	fileType := flag.String("fileType", "gob", "Either gob or csv to save a go binary file or csv")
 
 	flag.Parse()
 
 	// Ensure required file paths are provided
-	if *trainFile == "" || *testFile == "" || *trainSave == "" || *testSave == "" {
+	if *file == "" || (*csvSave == "" && *gobSave == "") {
 		flag.PrintDefaults()
 		os.Exit(1)
-	}
-
-	if *fileType != "gob" && *fileType != "csv" {
-		log.Fatal("Error: fileType must be one of gob or csv")
 	}
 
 	// Validate minDiff range
@@ -36,31 +29,21 @@ func main() {
 		log.Fatal("Error: Minimum relevance difference must be between 1 and 4")
 	}
 
-	XTrain, YTrain, err := datagen.CreateExamples(*trainFile, *trainCount, *minDiff)
+	X, Y, err := datagen.CreateExamples(*file, *exampleCount, *minDiff)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	XTest, YTest, err := datagen.CreateExamples(*trainFile, *testCount, *minDiff)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if *fileType == "gob" {
-		err = datagen.SaveData(*trainSave, XTrain, YTrain)
+	if *gobSave != "" {
+		err = datagen.SaveData(*gobSave, X, Y)
 		if err != nil {
 			return
 		}
-		err = datagen.SaveData(*testSave, XTest, YTest)
+	} else if *csvSave != "" {
 		if err != nil {
 			return
 		}
-	} else if *fileType == "csv" {
-		err = datagen.SaveDataToCSV(*trainSave, XTrain, YTrain)
-		if err != nil {
-			return
-		}
-		err = datagen.SaveDataToCSV(*testSave, XTest, YTest)
+		err = datagen.SaveDataToCSV(*csvSave, X, Y)
 		if err != nil {
 			return
 		}
